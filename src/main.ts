b0748c71d8b0f6b69b798f1672403ef566c52ad8
Asit Behera <cba.ab18@gmail.com>
2025-05-30 08:00:05 +0530

    Docker local setup completed

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const configService = app.get(ConfigService);
+  const logger = new Logger('Bootstrap');
+
+  const PORT = configService.get<number>('PORT') || 3000;
+  logger.log(`PORT :: ${PORT}`);
+
+  await app.listen(PORT);
 }
 bootstrap();
